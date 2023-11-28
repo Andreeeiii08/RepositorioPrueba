ef08f117942986f8e3bacfe60dcb02e9fde342cd
Andreeeiii08 <andrei.roshu@gmail.com>
2023-11-28 10:27:59 +0100

    Pasamos de 50 a 10 las repeticiones.

--- hola_mundo.py
+++ hola_mundo.py
@@ -1,3 +1,3 @@
 print("Hola, mundo, he modificado este archivo desde GitHub.")
-for i in range(0, 50):
-   print ("No copiaré en clase de programación.")
+for i in range(0, 10):
+    print ("No copiaré en clase de programación.")
